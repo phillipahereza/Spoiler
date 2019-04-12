b5c83b2969164b05b4818de815fef511c43acc7d
Phillip Ahereza <pahereza@gmail.com>
2019-04-12 13:17:31 +0200

    add victimform to homepage

--- tv_spoiler/views.py
+++ tv_spoiler/views.py
@@ -1,15 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import FormView, TemplateView
+from django.views.generic import FormView
 
 # Create your views here.
 
-class HomeView(TemplateView):
+class HomeView(FormView):
     pass
 
 
 class SpoilView(FormView):
     pass
 
-
-class AddVictimView(FormView):
-    pass
